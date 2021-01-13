116b2e9265b5f720667df24b470cb28cf9f55d57
daniel156161 <d.dolezal97@protonmail.com>
2021-01-13 17:55:22 +0100

    Add Clock to msg

--- js/msg.js
+++ js/msg.js
@@ -18,12 +18,23 @@ if(day === 31 && month === 12) {
 }
 //My Age or Normal msg
 if(msg.innerHTML === 'A <strong>responsive</strong> website created by me.'){
-  const rn = Math.floor(Math.random() * 2);
-  if(rn >= 1) {
+  const rn = Math.floor(Math.random() * 3);
+  if(rn == 1) {
     setInterval(() => {
       const time = (new Date() - new Date(874411200000)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
       const agetime = time.toString().substring(0, 12);
-      msg.innerHTML = "I'm a " + agetime + " year-old developer";
+      msg.innerHTML = `I'm a ${agetime} year-old developer`;
     }, 50);
+  } else if (rn == 2) {
+    setInterval(() => {
+      const time = new Date()
+      let hour = time.getHours()
+      let min = time.getMinutes()
+      let sec = time.getSeconds()
+      if (hour < 10) {  hour = '0' + hour; }
+      if (min < 10)  {  min = '0' + min;   }
+      if (sec < 10)  {  sec = '0' + sec;   }
+      msg.innerHTML = `Your Time is ${hour}:${min}:${sec} now` 
+    }, 1000);
   }
 }
